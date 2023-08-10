3f0674e8cf6fd56e97a8d5cd7b19960445e0ef61
anantsharma53@gmail.com <anantsharma53@gmail.com>
2023-08-10 15:48:47 +0530

    2nd

--- ecommercebackend/settings.py
+++ ecommercebackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-57va2t!kk*#(@62k-b865hvm&bgif_pb!6acf6v#)=8@3_5#w$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.231.233.98"]
 
 
 # Application definition
